7148db23feba9accdd290214711dd29a7ce2e888
CedarBishop <cedar.bishop@outlook.com>
2020-05-07 16:41:24 +1000

    Created Part 2 of turorial and removed unity example ML scenes as they are not compatible
    Created Penguin Agent script which recieves actions from neural network and sends back a reward value from -1 to 1
    Created penguin area which defines the game logic
    Created Fish script which acts as a moving target for the penguin agent

---
+++ Assets/Scripts/PenguinAgent.cs
@@ -0,0 +1,170 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PenguinAgent : Agent
+{
+    public float movementSpeed = 5.0f;
+    public float turnSpeed = 180.0f;
+    public GameObject heartPrefab;
+    public GameObject regurgitatedFishPrefab;
+
+    private PenguinArea penguinArea;
+    private Rigidbody rigidbody;
+    private GameObject baby;
+    private bool isFull;
+    private float feedRadius = 0.0f;
+
+    public override void InitializeAgent()
+    {
+        base.InitializeAgent();
+        penguinArea = GetComponentInParent<PenguinArea>();
+        baby = penguinArea.penguinBaby;
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+
+
+    public override void AgentAction(float[] vectorAction)
+    {
+        float forwardAmount = vectorAction[0];
+
+        float turnAmount = 0.0f;
+        if (vectorAction[1] == 1.0f)
+        {
+            turnAmount = -1.0f;
+        }
+        else if (vectorAction[1] == 2.0f)
+        {
+            turnAmount = -1.0f;
+        }
+
+        rigidbody.MovePosition(transform.position + transform.forward * forwardAmount * movementSpeed * Time.fixedDeltaTime);
+        transform.Rotate(transform.up * turnAmount * turnSpeed * Time.fixedDeltaTime);
+
+        if (maxStep > 0)
+        {
+            AddReward(-1.0f / maxStep);
+        }
+    }
+
+    
+
+    public override float[] Heuristic()
+    {
+        float forwardAction = 0.0f;
+        float turnAction = 0.0f;
+
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            // move forward
+            forwardAction = 1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            // turn left
+            turnAction = 1f;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            // turn right
+            turnAction = 2f;
+        }
+
+        return new float[] { forwardAction, turnAction };
+
+    }
+
+    public override void AgentReset()
+    {
+        isFull = false;
+        penguinArea.ResetArea();
+        feedRadius = Academy.Instance.FloatProperties.GetPropertyWithDefault("feed_radius", 0f);
+    }
+
+    public override void CollectObservations()
+    {
+        // has penguin eaten yet
+        AddVectorObs(isFull);
+        // distance to baby from adult penguin
+        AddVectorObs(Vector3.Distance(baby.transform.position, transform.position));
+        // direction to baby
+        AddVectorObs((baby.transform.position - transform.position).normalized);
+        // adult penguins current direction
+        AddVectorObs(transform.forward);
+
+        // 8 total values
+    }
+
+    private void FixedUpdate()
+    {
+        if (GetStepCount() % 5 == 0)
+        {
+            RequestDecision();
+        }
+        else
+        {
+            RequestAction();
+        }
+
+
+        if (Vector3.Distance(baby.transform.position, transform.position) < feedRadius)
+        {
+            RegurgitateFish();
+        }
+    }
+
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Fish")
+        {
+            EatFish(collision.gameObject);
+        }
+        else if (collision.transform.tag == "Baby")
+        {
+            RegurgitateFish();
+        }
+    }
+
+    void EatFish(GameObject fish)
+    {
+        if (isFull)
+        {
+            return;
+        }
+
+        isFull = true;
+        penguinArea.RemoveSpecificFish(fish);
+
+        AddReward(1.0f);
+
+    }
+
+    void RegurgitateFish()
+    {
+        if (isFull == false)
+        {
+            return;
+        }
+
+        isFull = false;
+
+        GameObject regurgitatedFish = Instantiate(regurgitatedFishPrefab,baby.transform.position, Quaternion.identity);
+        Destroy(regurgitatedFish,4.0f);
+
+        GameObject heart = Instantiate(heartPrefab, baby.transform.position + Vector3.up, Quaternion.identity);
+        Destroy(heart, 4.0f);
+
+        AddReward(1.0f);
+
+        if (penguinArea.FishRemaining() <= 0)
+        {
+            Done();
+        }
+    }
+}
---
+++ Assets/Scripts/PenguinArea.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+using TMPro;
+
+public class PenguinArea : MonoBehaviour
+{
+    public PenguinAgent penguinAgent;
+
+    public GameObject penguinBaby;
+
+    public TextMeshPro rewardText;
+
+    public Fish fishPrefab;
+
+    private List<GameObject> fishes = new List<GameObject>();
+
+    private void Start()
+    {
+        ResetArea();
+    }
+
+    private void Update()
+    {
+        rewardText.text = penguinAgent.GetCumulativeReward().ToString("F2");
+    }
+
+    public void ResetArea()
+    {
+        RemoveAllFish();
+        PlacePenguin();
+        PlaceBaby();
+        SpawnFish(4, Academy.Instance.FloatProperties.GetPropertyWithDefault("fish_speed", 0.5f));
+        
+    }
+
+    public void RemoveSpecificFish(GameObject fish)
+    {
+        fishes.Remove(fish);
+        Destroy(fish);
+    }
+
+    public int FishRemaining ()
+    {
+        return fishes.Count;
+    }
+
+    public static Vector3 ChooseRandomPosition (Vector3 centre, float minAngle, float maxAngle, float minRadius, float maxRadius)
+    {
+        float radius = minRadius;
+        float angle = minAngle;
+
+        if (maxRadius > minRadius)
+        {
+            radius = Random.Range(minRadius, maxRadius);
+        }
+
+        if (maxAngle > minAngle)
+        {
+            angle = Random.Range(minAngle,maxAngle);
+        }
+
+        return centre + Quaternion.Euler(0.0f, angle, 0.0f) * Vector3.forward * radius;
+    }
+
+    private void RemoveAllFish()
+    {
+        if (fishes != null)
+        {
+            foreach (GameObject fish in fishes)
+            {
+                Destroy(fish);
+            }
+        }
+
+        fishes.Clear();
+    }
+
+    private void PlacePenguin()
+    {
+        Rigidbody rigidbody = penguinAgent.GetComponent<Rigidbody>();
+        rigidbody.velocity = Vector3.zero;
+        rigidbody.angularVelocity = Vector3.zero;
+        penguinAgent.transform.position = ChooseRandomPosition(transform.position, 0.0f ,360.0f, 0.0f, 9.0f) + Vector3.up * 0.5f;
+        penguinAgent.transform.rotation = Quaternion.Euler(0.0f, Random.Range(0.0f, 360.0f), 0.0f);
+    }
+
+
+    private void PlaceBaby()
+    {
+        Rigidbody rigidbody = penguinBaby.GetComponent<Rigidbody>();
+        rigidbody.velocity = Vector3.zero;
+        rigidbody.angularVelocity = Vector3.zero;
+        penguinBaby.transform.position = ChooseRandomPosition(transform.position, -45f, 45f, 4f, 9f) + Vector3.up * .5f;
+        penguinBaby.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+    }
+
+    private void SpawnFish(int count, float fishSpeed)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            Fish fish = Instantiate(
+                fishPrefab,
+                ChooseRandomPosition(transform.position, 100.0f, 260.0f, 2.0f, 13.0f) + Vector3.up * 0.5f,
+                Quaternion.Euler(0.0f, Random.Range(0.0f, 360.0f) , 0.0f)
+                );
+
+            fishes.Add(fish.gameObject);
+
+            //fish.fishSpeed = fishSpeed;
+        }
+    }
+}
